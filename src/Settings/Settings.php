bbbe73d9d134b4bc0b2715847a099c4d3882df6d
oleg khrulev <linqur@gmail.com>
2022-09-12 16:32:06 +0300

    Содал структуру настроек

---
+++ src/Settings/Settings.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Linqur\IikoService\Settings;
+
+class Settings
+{
+    
+}
---
+++ src/Settings/SettingsValuesInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Linqur\IikoService\Settings;
+
+interface SettingsValuesInterface
+{
+    /**
+     * Получить api логин
+     * 
+     * @return string
+     */
+    public function getApiLogin();
+}
