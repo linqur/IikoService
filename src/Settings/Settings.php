3de0649e8b8c567fef3b849dedcf008a1247ec98
oleg khrulev <linqur@gmail.com>
2022-09-12 16:35:12 +0300

    Добавил обработку данных settings

--- src/Settings/Settings.php
+++ src/Settings/Settings.php
@@ -2,7 +2,22 @@
 
 namespace Linqur\IikoService\Settings;
 
+use Linqur\Singleton\SingletonTrait;
+
 class Settings
 {
-    
+    use SingletonTrait;
+    /** @var SettingsValuesInterface */
+    private $values;
+
+    /** @param SettingsValuesInterface $values */
+    public function setValues($values)
+    {
+        $this->values = $values;
+    }
+
+    public function getApiLogin()
+    {
+        return $this->values->getApiLogin();
+    }
 }
