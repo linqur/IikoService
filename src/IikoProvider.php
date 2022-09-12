e424212bb3061744356f028c0d71aa04cb4f3bce
oleg khrulev <linqur@gmail.com>
2022-09-12 16:31:55 +0300

    Добавил провайдер

---
+++ src/IikoProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Linqur\IikoService;
+
+use Linqur\Singleton\SingletonTrait;
+
+class IikoProvider
+{
+    use SingletonTrait;
+
+    /**
+     * 
+     */
+    public function setSettings()
+    {
+
+    }
+}
